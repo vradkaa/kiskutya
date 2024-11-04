222714f072710ac31659a96664ced1e2bf8dd1d1
Violet Wildest <thewildestviolet@Violet-MacBook-Air.local>
2024-11-04 13:28:49 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿List<double> poly = [];
+string? input;
+int count = 1;
+
+while (true)
+{
+    Console.WriteLine($"Coordinate {count} X:");
+    input = Console.ReadLine();
+    if (!double.TryParse(input, out double x))
+    {
+        break;
+    }
+
+    Console.WriteLine($"Coordinate {count} Y:");
+    input = Console.ReadLine();
+    if (!double.TryParse(input, out double y))
+    {
+        break;
+    }
+
+    poly.Add(x);
+    poly.Add(y);
+
+    count++;
+}
+
+Console.WriteLine(string.Join(',', poly));
